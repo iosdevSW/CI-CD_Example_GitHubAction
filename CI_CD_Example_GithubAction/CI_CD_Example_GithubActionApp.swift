3cb1479b71f7a52a4edb4f4b0bec45e2d620d6c9
iosdevSW <iosdev.sw@gmail.com>
2024-02-24 14:55:43 +0900

    Initial Commit

---
+++ CI_CD_Example_GithubAction/CI_CD_Example_GithubActionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CI_CD_Example_GithubActionApp.swift
+//  CI_CD_Example_GithubAction
+//
+//  Created by iOS신상우 on 2/24/24.
+//
+
+import SwiftUI
+
+@main
+struct CI_CD_Example_GithubActionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CI_CD_Example_GithubAction/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CI_CD_Example_GithubAction
+//
+//  Created by iOS신상우 on 2/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
